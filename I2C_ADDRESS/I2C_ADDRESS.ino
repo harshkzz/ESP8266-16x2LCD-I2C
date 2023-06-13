7d98605608cbbabc789d405121e75813b2786758
Harsh Kumar <125388784+harshkzz@users.noreply.github.com>
2023-06-13 23:45:19 +0530

    Add files via upload

---
+++ I2C_ADDRESS/I2C_ADDRESS.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+void setup() {
+  Serial.begin (9600);
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+  // 0x27 0x3F
+  Wire.begin();
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+      {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);
+      }
+  }
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+}  // end of setup
+
+void loop() {}
